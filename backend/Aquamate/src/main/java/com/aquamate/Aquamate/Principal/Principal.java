fbab5baf14a95770959e8e7267026acb1c7ec1c3
André Wanderley <andrewanderleyporto@gmail.com>
2024-05-04 13:00:06 -0300

    comentario teste

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/Principal/Principal.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/Principal/Principal.java
@@ -3,6 +3,7 @@ package com.aquamate.Aquamate.Principal;
 public class Principal {
     public void TestePrincipal() {
         System.out.println("Hello World");
-        /* alo */
+        /* alo, wanderley aq */
+
     }
 }
