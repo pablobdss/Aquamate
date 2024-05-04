1db746c636c4156acb861cbabf091890a40fc896
Juan Pablo <juanpablox2002@gmail.com>
2024-05-04 10:24:15 -0300

    SpringBoot Base

---
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/AquamateApplication.java
@@ -0,0 +1,13 @@
+package com.aquamate.Aquamate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquamateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquamateApplication.class, args);
+	}
+
+}
