67926f4ccdb0b8e629698bcc553aafd04d26a7c2
Juan Pablo <juanpablox2002@gmail.com>
2024-05-04 13:03:34 -0300

    CommandLiner adicionado para executar um Run Void a parte

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/AquamateApplication.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/AquamateApplication.java
@@ -1,17 +1,25 @@
 package com.aquamate.Aquamate;
 
-import com.aquamate.Aquamate.Principal.Principal;
+import com.aquamate.Aquamate.principal.Principal;
+import com.aquamate.Aquamate.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AquamateApplication {
+public class AquamateApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AquamateApplication.class, args);
+	}
 
-		// Criar uma instância de Principal e chamar o método TestePrincipal
-		Principal principal = new Principal();
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repository);
 		principal.TestePrincipal();
 	}
 }
