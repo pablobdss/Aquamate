9d8b104e0c1dc0bca188eabea4eb1d239f2fa93a
Juan Pablo <juanpablox2002@gmail.com>
2024-05-15 12:27:42 -0300

    Adicionado comentários explicando o CorsConfig

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/config/CorsConfiguration.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/config/CorsConfiguration.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
+@Configuration // Marca esta classe como uma configuração do Spring.
+public class CorsConfiguration implements WebMvcConfigurer { // Implementa WebMvcConfigurer para customizar o comportamento padrão do Spring MVC.
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5501", "http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    @Override // Sobrescreve o método da interface WebMvcConfigurer.
+    public void addCorsMappings(CorsRegistry registry) { // Define as regras de CORS.
+        registry.addMapping("/**") // Permite CORS para todas as rotas (/**) da aplicação.
+                .allowedOrigins("http://127.0.0.1:5501", "http://127.0.0.1:5500") // Permite requisições de CORS nos domínios especificados.
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"); // Metodos HTTP permitids nas requisições CORS.
     }
 }
