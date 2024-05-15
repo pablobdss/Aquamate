c8e76ecc674db6c6df8060a307f0d84488db691f
Juan Pablo <juanpablox2002@gmail.com>
2024-05-15 12:19:43 -0300

    Ajuste no DTO

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/dto/UsuarioDTO.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/dto/UsuarioDTO.java
@@ -1,24 +1,6 @@
 package com.aquamate.Aquamate.dto;
 
-
-import com.aquamate.Aquamate.model.Usuario;
-
-public record UsuarioDTO (String email,
-                          String senha){
-
-    @Override
-    public boolean equals(Object obj) {
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return 0;
-    }
-
-    @Override
-    public String toString() {
-        return "";
-    }
+public record UsuarioDTO (String email, String senha){
+    
 }
 
