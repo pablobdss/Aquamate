20b02a5fe97c44027f8ad0321248fe44c744b082
Juan Pablo <juanpablox2002@gmail.com>
2024-05-16 19:21:32 -0300

    DadosUsuarioController criado
    Controller esta utilizando usuario dentro para testes, devido a bugs do UsuarioController

---
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/controller/DadosUsuarioController.java
@@ -0,0 +1,89 @@
+package com.aquamate.Aquamate.controller;
+
+import com.aquamate.Aquamate.dto.DadosUsuarioDTO;
+import com.aquamate.Aquamate.dto.UsuarioDTO;
+import com.aquamate.Aquamate.model.DadosUsuario;
+import com.aquamate.Aquamate.model.Usuario;
+import com.aquamate.Aquamate.repository.DadosUsuarioRepository;
+import com.aquamate.Aquamate.repository.UsuarioRepository;
+import com.aquamate.Aquamate.service.CadastroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping ("/dadosUsuario")
+public class DadosUsuarioController {
+
+    @Autowired
+    private DadosUsuarioRepository dadosRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private CadastroService cadastroService;
+
+    Usuario usuario = new Usuario();
+
+    @GetMapping
+    public ResponseEntity getAllDados() {
+        var allDados = dadosRepository.findAll();
+        return ResponseEntity.ok(allDados);
+    }
+
+    @GetMapping("/usuarios")
+    public ResponseEntity getAllUsers() {
+        var allUsers = usuarioRepository.findAll();
+        return ResponseEntity.ok(allUsers);
+    }
+
+    @PostMapping("/usuario")
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioSalvo = cadastroService.cadastrarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSalvo);
+    }
+
+    @PostMapping("/{id_usuario}/post")
+    public ResponseEntity<DadosUsuario> cadastrarDadosUsuario(@PathVariable Long id_usuario, @RequestBody DadosUsuario dadosUsuario) {
+        DadosUsuario dadosUsuarioSalvo = cadastroService.cadastrarDadosUsuario(id_usuario, dadosUsuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dadosUsuarioSalvo);
+    }
+
+    @PostMapping
+    public ResponseEntity postDados(@RequestBody @Validated DadosUsuarioDTO data) {
+        DadosUsuario dadosUsuario = new DadosUsuario(data);
+        System.out.println(data);
+        dadosRepository.save(dadosUsuario);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping()
+    public ResponseEntity updateDados(@RequestBody @Validated DadosUsuarioDTO data) {
+        Optional<DadosUsuario> dadosUsuarioOptional = dadosRepository.findById(data.id());
+        var usuario = new Usuario();
+        if (dadosUsuarioOptional.isPresent()) {
+            DadosUsuario dadosUsuario = dadosUsuarioOptional.get();
+
+            // Atualize os campos do dadosUsuario com base nos dados recebidos no DTO
+            dadosUsuario.setApelido(data.apelido());
+            dadosUsuario.setPeso(data.peso());
+            dadosUsuario.setIdade(data.idade());
+            dadosUsuario.setAltura(data.altura());
+            dadosUsuario.setTipoMeta(data.tipoMeta());
+            dadosUsuario.setDataNascimento(data.dataNascimento());
+
+            // Salve as alterações no repositório
+            dadosRepository.save(dadosUsuario);
+
+            return ResponseEntity.ok(dadosUsuario);
+        } else {
+            return ResponseEntity.notFound().build(); // Retorna 404 caso o DadosUsuario não seja encontrado
+        }
+    }
+
+}
