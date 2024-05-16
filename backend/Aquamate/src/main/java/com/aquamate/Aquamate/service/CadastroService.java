3055db71b1efb64cfc85c7a80e3677756cbe5bc6
Juan Pablo <juanpablox2002@gmail.com>
2024-05-16 19:16:40 -0300

    Service de Cadastro para teste

---
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/service/CadastroService.java
@@ -0,0 +1,31 @@
+package com.aquamate.Aquamate.service;
+
+import com.aquamate.Aquamate.model.DadosUsuario;
+import com.aquamate.Aquamate.model.Usuario;
+import com.aquamate.Aquamate.repository.DadosUsuarioRepository;
+import com.aquamate.Aquamate.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class CadastroService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private DadosUsuarioRepository dadosUsuarioRepository;
+
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public DadosUsuario cadastrarDadosUsuario(Long idUsuario, DadosUsuario dadosUsuario) {
+        Usuario usuario = usuarioRepository.findById(idUsuario)
+                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado com o ID: " + idUsuario));
+        dadosUsuario.setUsuario(usuario);
+        return dadosUsuarioRepository.save(dadosUsuario);
+    }
+}
+
