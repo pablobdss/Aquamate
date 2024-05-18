17e7da4654d526688ebe750903935210270a9360
Juan Pablo <juanpablox2002@gmail.com>
2024-05-18 12:13:20 -0300

    Novos Controllers para o dashboard

---
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/controller/MetaController.java
@@ -0,0 +1,9 @@
+package com.aquamate.Aquamate.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Meta")
+public class MetaController {
+}
---
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/controller/RegistroController.java
@@ -0,0 +1,10 @@
+package com.aquamate.Aquamate.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registroConsumo")
+public class RegistroController {
+
+}
