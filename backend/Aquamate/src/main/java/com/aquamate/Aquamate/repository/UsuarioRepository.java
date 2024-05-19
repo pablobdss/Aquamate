91d34962621a1a04f603f1c116d91fda8a5956e2
Juan Pablo <juanpablox2002@gmail.com>
2024-05-19 18:19:25 -0300

    modificado para Optional

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/repository/UsuarioRepository.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/repository/UsuarioRepository.java
@@ -2,9 +2,9 @@ package com.aquamate.Aquamate.repository;
 
 import com.aquamate.Aquamate.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 }
