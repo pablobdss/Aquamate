27aacebe1f42577f10dcac37decb77d40709dbeb
Juan Pablo <75508836+pablobdss@users.noreply.github.com>
2024-05-19 17:58:18 -0300

    JSONPROPERTIES IGNORE

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/model/MetaAuto.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/model/MetaAuto.java
@@ -1,5 +1,6 @@
 package com.aquamate.Aquamate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,6 +11,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@JsonIgnoreProperties("dadosUsuario")
 public class MetaAuto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
