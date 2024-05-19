573f9ec789d12ada561b8dbbbf6e7f3c0e0896ef
guima11 <105176137+guima11@users.noreply.github.com>
2024-05-19 09:48:28 -0300

    Fix: ajustes nos testes

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/controller/UsuarioController.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/controller/UsuarioController.java
@@ -3,27 +3,20 @@ package com.aquamate.Aquamate.controller;
 
 import com.aquamate.Aquamate.dto.UsuarioDTO;
 import com.aquamate.Aquamate.model.Usuario;
-import com.aquamate.Aquamate.repository.UsuarioRepository;
 import com.aquamate.Aquamate.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.AuthenticationException;
 
 import static org.springframework.http.HttpStatus.*;
-
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-
-    @Autowired
-    private UsuarioService usuarioService;
-
-    Usuario usuario = new Usuario();
+    private final UsuarioService usuarioService;
 
     @Autowired
     public UsuarioController(UsuarioService usuarioService) {
@@ -42,11 +35,10 @@ public class UsuarioController {
     @PostMapping("/login")
     public ResponseEntity<Object> fazerLogin(@RequestBody @Validated UsuarioDTO usuarioDTO) {
         try {
-            // Realiza o login e retorna o usuário (sem a senha) caso seja bem-sucedido
             Usuario usuario = usuarioService.fazerLogin(usuarioDTO.email(), usuarioDTO.senha());
             usuario.setSenha(null); // Remove a senha antes de enviar como resposta
             return ResponseEntity.ok(usuario);
-        } catch (Exception e) {
+        } catch (AuthenticationException e) {
             return ResponseEntity.status(UNAUTHORIZED).body("Credenciais inválidas");
         }
     }
