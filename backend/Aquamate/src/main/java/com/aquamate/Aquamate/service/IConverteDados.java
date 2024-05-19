6cfc908eaeaf810d38bcd3ee68802b8df946e001
Juan Pablo <juanpablox2002@gmail.com>
2024-05-19 19:38:49 -0300

    fetched

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/service/IConverteDados.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/service/IConverteDados.java
@@ -2,7 +2,9 @@ package com.aquamate.Aquamate.service;
 
 import com.aquamate.Aquamate.dto.UsuarioDTO;
 import com.aquamate.Aquamate.model.Usuario;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface IConverteDados {
     <T> T  obterDados(String json, Class<T> classe);
 
