56f379f15a31ed8f84c700a8e7a90e880a574573
Juan Pablo <75508836+pablobdss@users.noreply.github.com>
2024-05-19 21:29:59 -0300

    Delete backend directory

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aquamate.Aquamate.repository;
-
-import com.aquamate.Aquamate.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/service/IConverteDados.java
+++
@@ -1,5 +0,0 @@
-package com.aquamate.Aquamate.service;
-
-public interface IConverteDados {
-    <T> T  obterDados(String json, Class<T> classe);
-}
