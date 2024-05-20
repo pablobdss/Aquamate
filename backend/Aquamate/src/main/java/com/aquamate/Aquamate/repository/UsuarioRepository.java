004ba32f62f499fbaea6fd47112758931328681c
Juan Pablo <juanpablox2002@gmail.com>
2024-05-19 21:43:17 -0300

    Reupdate

--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/repository/UsuarioRepository.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package com.aquamate.Aquamate.repository;
 import com.aquamate.Aquamate.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
--- backend/Aquamate/src/main/java/com/aquamate/Aquamate/service/IConverteDados.java
+++ backend/Aquamate/src/main/java/com/aquamate/Aquamate/service/IConverteDados.java
@@ -1,5 +1,14 @@
 package com.aquamate.Aquamate.service;
 
+import com.aquamate.Aquamate.dto.UsuarioDTO;
+import com.aquamate.Aquamate.model.Usuario;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface IConverteDados {
     <T> T  obterDados(String json, Class<T> classe);
+
+    Usuario converterParaEntidade(UsuarioDTO usuarioDTO);
+
+    UsuarioDTO converterParaDTO(Usuario usuario);
 }
