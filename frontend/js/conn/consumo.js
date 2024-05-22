10a5a14994b293fd91b5264a39f7504150f8ceed
Juan Pablo <juanpablox2002@gmail.com>
2024-05-21 21:03:22 -0300

    Separação de Responsabilidades
    Os arquivos JS agora foram separados entre os métodos e suas conexões com API, estarão agora na pasta conn todas requisições a API

---
+++ frontend/js/conn/consumo.js
@@ -0,0 +1,29 @@
+export default async function getConsumo() {
+    const id_dadosUsuario = 1; // Substitua 1 pelo ID do usuário correto
+    const url = `http://localhost:8080/consumo/resgatar?id_dadosUsuario=${id_dadosUsuario}`;
+
+    return fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Erro ao obter consumo atual de água');
+            }
+        });
+}
+
+export function atualizarConsumo(data) {
+    const id_dadosUsuario = 1;
+    const url = `http://localhost:8080/consumo/atualizar?id_dadosUsuario=${id_dadosUsuario}`;
+
+    console.log('Enviando solicitação PUT para:', url);
+    console.log('Dados a serem enviados:', JSON.stringify(data));
+
+    return fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+}
