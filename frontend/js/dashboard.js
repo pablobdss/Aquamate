173b484be2831faeee0bfefdb019a34edd5fa98a
Juan Pablo <juanpablox2002@gmail.com>
2024-05-21 21:11:21 -0300

    Update Dashboard
    Cone de agua que vai descendo conforme a porcentagem cresce

---
+++ frontend/js/dashboard.js
@@ -0,0 +1,26 @@
+
+import getConsumo from "./conn/consumo.js";
+function obterConsumoAtual() {
+    getConsumo()
+        .then(data => {
+            console.log('Dados recebidos:', data);
+            const percentualAtingido = data.percentualAtingido;
+            console.log('Percentual Atingido:', percentualAtingido);
+
+            // Atualize a interface com o percentual atingido
+            const consumoAtualElement = document.getElementById("consumoAtual");
+            consumoAtualElement.textContent = `Percentual Atingido: ${percentualAtingido}%`;
+
+            // Converte percentualAtingido para fração decimal
+            const waterLevel = percentualAtingido / 100;
+            
+            // Atualiza o estilo da água
+            document.querySelector('.water').style.setProperty('--water-level', waterLevel);
+        })
+        .catch(error => {
+            console.error('Erro ao obter consumo atual de água:', error);
+        });
+}
+
+// Chama a função ao carregar a página
+window.onload = obterConsumoAtual;
