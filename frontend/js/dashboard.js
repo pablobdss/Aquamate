99053104b7e1539a0d2e1363e3d7859a41c4203f
fabiords07 <142637818+fabiords07@users.noreply.github.com>
2024-05-23 19:18:54 -0300

    função para evitar acesso sem login

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -33,3 +33,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
         element.addEventListener('mouseout', grow);
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM completamente carregado e analisado');
+
+    function verificarLogin() {
+        const authToken = localStorage.getItem('authToken');
+        console.log('AuthToken:', authToken);
+        return authToken !== null;
+    }
+
+    if (verificarLogin()) {
+        console.log('Usuário está logado');
+        const logoutLink = document.getElementById('link2');
+        if (logoutLink) {
+            logoutLink.addEventListener('click', function(event) {
+                event.preventDefault();
+                console.log('Link de logout clicado');
+                fetch('http://localhost:8080/usuario/logout', {
+                    method: 'POST',
+                    credentials: 'include'
+                })
+                .then(response => {
+                    if (response.ok) {
+                        console.log('Logout bem-sucedido');
+                        localStorage.removeItem('authToken');
+                        window.location.href = '/frontend/pages/login-register/login-register.html';
+                    } else {
+                        console.error('Erro ao fazer logout', response.status, response.statusText);
+                    }
+                })
+                .catch(error => {
+                    console.error('Erro na requisição de logout:', error);
+                });
+            });
+        } else {
+            console.error('Link de logout não encontrado no DOM');
+        }
+    } else {
+        console.log('Usuário não está logado');
+    }
+});
